9715d976ecfc442694f04a420ec39df532cd9ed0
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2018-12-14 11:30:06 +0100

    use gatsby-node.js to foward codesandbox to gatsby

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,30 @@
  */
 
 // You can delete this file if you're not using it
+
+const fs = require('fs')
+
+exports.onPreBootstrap = () => {
+  console.log('OnPreBootstrap ------------------')
+  // for each secrets (key/value) hosted on codesansbox, push them in a secrets string
+  let secrets = ''
+  for (var propertyName in process.env) {
+    if (
+      process.env.hasOwnProperty(propertyName) &&
+      ~propertyName.indexOf('kdo_')
+    ) {
+      secrets += `${propertyName}=${process.env[propertyName]}\n`
+    }
+  }
+
+  // then write this string to .env.development file, which allows env var to to used in gatsby project
+  fs.writeFile('.env.development', secrets, err => {
+    if (err) throw err
+    console.log('Dev env var saved!')
+  })
+
+  fs.writeFile('.env.production', secrets, err => {
+    if (err) throw err
+    console.log('Prod env var saved!')
+  })
+}
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -8,8 +8,9 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <p>Welcome to page 2 </p>
+    <Link to="/">Go back to the homepagem</Link>
+    <button onClick={() => console.log(process.env.kdo_apiKey)}>click</button>
   </Layout>
 )
 
