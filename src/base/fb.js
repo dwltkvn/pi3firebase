a937968bd928407e380861e9f7af747847beb880
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2018-12-18 00:23:33 +0100

    add first sample of a firebase/firestore

--- src/base/fb.js
+++ src/base/fb.js
@@ -1,12 +1,13 @@
-import firebase from 'firebase';
+import firebase from 'firebase'
 
-const config = { 
-      apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
-};
-firebase.initializeApp(config);
-export default firebase;
+const config = {
+  apiKey: process.env.kdo_apiKey,
+  authDomain: process.env.kdo_authDomain,
+  databaseURL: process.env.kdo_databaseURL,
+  projectId: process.env.kdo_projectId,
+  storageBucket: process.env.kdo_storageBucket,
+  messagingSenderId: process.env.kdo_messagingSenderId,
+}
+
+firebase.initializeApp(config)
+export default firebase
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,6 +3,26 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import fb from '../base/fb'
+
+function test()
+{
+  let db = fb.firestore();
+  let user1Ref = db.collection("users").doc("alovelace");
+  user1Ref.get()
+          .then( (doc) => {
+                            if(doc.exists){
+                              console.log(doc.data())
+                            }
+                          })
+          .catch( (error) => console.log(error) );
+  //console.log(user1Ref);
+  /*db.collection("users").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
+});*/
+}
 
 const SecondPage = () => (
   <Layout>
@@ -11,6 +31,7 @@ const SecondPage = () => (
     <p>Welcome to page 2 </p>
     <Link to="/">Go back to the homepagem</Link>
     <button onClick={() => console.log(process.env.kdo_apiKey)}>click</button>
+    <button onClick={() => test()}>click2</button>
   </Layout>
 )
 
