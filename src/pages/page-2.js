d2d250955299db0f9c224829c695fb27517e4443
Dwlt Kvn <7428297+dwltkvn@users.noreply.github.com>
2019-02-05 19:40:23 +0100

    change notification type
    notification works on desktop (chromeos) and mobile

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -38,12 +38,10 @@ function notifyMe() {
     console.log("notifyMe?2");
     //var notification = new Notification("Salut toi !");
     navigator.serviceWorker.ready.then(function(registration) {
-        registration.showNotification('Vibration Sample', {
-          body: 'Buzz! Buzz!',
-          //icon: '../images/touch/chrome-touch-icon-192x192.png',
-          vibrate: [200, 100, 200, 100, 200, 100, 200],
-          tag: 'vibration-sample'
-        });
+        console.log("permission already granted");
+        const title = 'Icon Notification';
+        const options = { };
+        registration.showNotification(title, options);
       });
   }
 
@@ -63,12 +61,10 @@ function notifyMe() {
       if (permission === "granted") {
         //var notification = new Notification("Salut toi !");
       navigator.serviceWorker.ready.then(function(registration) {
-        registration.showNotification('Vibration Sample', {
-          body: 'Buzz! Buzz!',
-          icon: '../images/touch/chrome-touch-icon-192x192.png',
-          vibrate: [200, 100, 200, 100, 200, 100, 200],
-          tag: 'vibration-sample'
-        });
+        console.log("permission granted");
+        const title = 'Icon Notification';
+        const options = { };
+        registration.showNotification(title, options);
       });
       }
     });
